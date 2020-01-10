6c68003a273890459680f25e558d68ea4ad729f4
timglabisch <tim@ainfach.de>
2020-01-10 22:09:45 +0100

    ...

--- php/foo.php
+++ php/foo.php
@@ -5,26 +5,5 @@ $ffi = \FFI::cdef(
     __DIR__ . '/../rlib/target/release/librlib.dylib'
 );
 
-/*
-$buffer = FFI::new("char[1024]");
-foreach (range(0, 1023) as $i) {
-    $buffer[$i] = 'a';
-}
-*/
-
-file_put_contents('/tmp/foo1.txt', 'one');
-file_put_contents('/tmp/foo2.txt', 'two');
-
-
-$ffi->start();
-echo "read one\n";
-echo $ffi->queue_read('/tmp/foo1.txt');
-
-/*
-echo "read two\n";
-echo $ffi->queue_read('/tmp/foo2.txt');
-echo "start polling\n";
-*/
-
 $poll1 = $ffi->poll();
 var_dump($poll1);
--- rlib/rlib.h
+++ rlib/rlib.h
@@ -3,7 +3,4 @@ struct Buffer {
     uint32_t   len; // u64 doesnt work?!
 };
 
-void start();
-uint64_t queue_read(char* filename);
-// void free_buf(struct buffer *buf);
 struct Buffer poll();
