f8447e96ba673a0c45939fa498b5ac5224d03860
timglabisch <tim@ainfach.de>
2020-01-15 21:38:30 +0100

    ...

--- examples/call_rust_func/exampleTest.php
+++ examples/call_rust_func/exampleTest.php
@@ -14,15 +14,22 @@ class exampleTest extends TestCase
 
     public function testReturnU64()
     {
-        static::assertSame(42, $this->ffi()->return_u64());
+        $ffi = $this->ffi();
+        static::assertSame(42, $ffi->return_u64());
     }
 
     public function testReturnPointer()
     {
-        $res = $this->ffi()->return_pointer();
+        $ffi = $this->ffi();
+        $res = $ffi->return_pointer();
 
-        var_dump($res);
-
-        // static::assertSame(1, $res->_1);
+        static::assertSame(1, $res[0]->_1);
+        static::assertSame(2, $res[0]->_2);
+        static::assertSame(3, $res[0]->_3);
+        static::assertSame(4, $res[0]->_4);
+        static::assertSame(5, $res[0]->_5);
+        static::assertSame(6, $res[0]->_6);
+        static::assertSame(7, $res[0]->_7);
+        static::assertSame(8, $res[0]->_8);
     }
 }
